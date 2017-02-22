087ed1e98aa652a631cc0c187ae4562a9c815240
Yuriy Klyuyko <Yuriy Klyuyko>
2017-02-23 00:47:33 +0200

    implement_loader_callback_methods

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -19,28 +19,47 @@ import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.example.android.pets.data.PetContract;
+
 import com.example.android.pets.data.PetContract.PetEntry;
-import com.example.android.pets.data.PetDbHelper;
+
 
 /**
  * Displays list of pets that were entered and stored in the app.
  */
-public class CatalogActivity extends AppCompatActivity {
+public class CatalogActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor>{
 
     private static final String LOG_TAG = CatalogActivity.class.getSimpleName();
+
+    // This is the Adapter being used to display the list's data.
+    SimpleCursorAdapter mAdapter;
+
+    // These are the pets rows that we will retrieve
+    static final String[] PROJECTION = new String[] {
+            PetEntry._ID,
+            PetEntry.COLUMN_PET_NAME,
+            PetEntry.COLUMN_PET_BREED
+    };
+
+    // This is the select criteria
+    static final String SELECTION = "(" + PetEntry.COLUMN_PET_NAME + "NOTNULL" + ")";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +76,32 @@ public class CatalogActivity extends AppCompatActivity {
             }
         });
 
-        displayDatabaseInfo();
+        // Find the ListView which will be populated with the pet data
+        ListView petListView = (ListView) findViewById(R.id.pets_list_view);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        // Set the view to show if the adapter is empty
+        petListView.setEmptyView(emptyView);
+
+        // For the cursor adapter, specify which columns go into which views
+        String[] fromColumns = {PetEntry.COLUMN_PET_NAME, PetEntry.COLUMN_PET_BREED};
+        int[] toViews = {R.id.name, R.id.summary}; // The TextView in list_item
+
+        // Create an empty adapter we will use to display the loaded data.
+        // We pass null for the cursor, then update it in onLoadFinished()
+        mAdapter = new SimpleCursorAdapter(
+                this,
+                R.layout.list_item,
+                null,
+                fromColumns,
+                toViews,
+                0
+        );
+
+        petListView.setAdapter(mAdapter);
+
+        getSupportLoaderManager().initLoader(0, null, this);
     }
 
     @Override
@@ -132,30 +176,54 @@ public class CatalogActivity extends AppCompatActivity {
             PetEntry.COLUMN_PET_WEIGHT
         };
 
-        Cursor cursor = null;
-        try {
-            // Query for items from the database and get a cursor back
-            cursor = getContentResolver().query(
-                    PetEntry.CONTENT_URI,
-                    projection,
-                    null,
-                    null,
-                    null
-            );
+        // Perform a query on the provider using the ContentResolver.
+        Cursor cursor = getContentResolver().query(
+                PetEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null
+        );
 
-            // Attaching the Adapter to a ListView
-            // Find ListView to populate
-            ListView lvItems = (ListView) findViewById(R.id.pets_list_view);
-            // Setup cursor adapter using cursor
-            PetCursorAdapter petAdapter = new PetCursorAdapter(this, cursor);
-            lvItems.setAdapter(petAdapter);
-        } finally {
-            cursor.close();
-        }
+        // Find the ListView which will be populated with the pet data
+        ListView lvItems = (ListView) findViewById(R.id.pets_list_view);
 
+        // Setup an Adapter to create a list item for each row of pet data in the Cursor.
+        PetCursorAdapter petAdapter = new PetCursorAdapter(this, cursor);
 
-
-
+        // Attach the adapter to the ListView
+        lvItems.setAdapter(petAdapter);
     }
 
+    // Called when a new Loader needs to be created
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed
+        return new CursorLoader(
+                this,
+                PetEntry.CONTENT_URI,
+                PROJECTION,
+                SELECTION,
+                null,
+                null
+        );
+    }
+
+    // Called when a previously created loader has finished loading
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Swap the new cursor in.  (The framework will take care of closing the
+        // old cursor once we return.)
+        mAdapter.swapCursor(data);
+    }
+
+    // Called when a previously created loader is reset, making the data unavailable
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // This is called when the last Cursor provided to onLoadFinished()
+        // above is about to be closed.  We need to make sure we are no
+        // longer using it.
+        mAdapter.swapCursor(null);
+    }
 }
